73584ecb73b26d61efd4688d0acc0ebe241f85f9
Davi Pedro <davisilvaps02@gmail.com>
2024-02-11 20:42:03 -0300

    Relacionando Items ao Auction

--- NLW-Leilao.API/Entities/Auction.cs
+++ NLW-Leilao.API/Entities/Auction.cs
@@ -5,5 +5,6 @@ public class Auction
     public int Id { get; set; }
     public string Name { get; set; } = String.Empty;
     public DateTime Starts { get; set; }
-    public DateTime Ends { get; set; } 
+    public DateTime Ends { get; set; }
+    public List<Item> Items { get; set; } = [];
 }
