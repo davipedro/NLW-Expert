9ffa416f1ec7993b7ed51f91af37abc1a38eee16
Davi Pedro <davisilvaps02@gmail.com>
2024-02-12 11:27:30 -0300

    Criando o OfferController

---
+++ NLW-Leilao.API/Controllers/OfferController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NLW_Leilao.API.Controllers;
+
+public class OfferController : NlwAuctionBaseController
+{
+    [HttpPost]
+    public IActionResult CreateOffer()
+    {
+        return Created();
+    }
+}
