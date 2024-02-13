21137aacfa02317e8f4d5702481c7d62365b28dd
Davi Pedro <davisilvaps02@gmail.com>
2024-02-12 21:03:24 -0300

    Criando Service padrão para configuração de ORM

---
+++ NLW-Leilao.API/Repositories/NlwAuctionBaseServiceConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace NLW_Leilao.API.Repositories;
+
+public abstract class NlwAuctionBaseServiceConfig : DbContext
+{
+    protected string ConnectionString;
+
+    protected NlwAuctionBaseServiceConfig()
+    {
+        ConnectionString = Environment.GetEnvironmentVariable("EF_NLW-AUCTION_CONNECTION_STRING");
+    }
+
+    protected NlwAuctionBaseServiceConfig(DbContextOptions<AuctionDbContext> options)
+    {
+    }
+}
